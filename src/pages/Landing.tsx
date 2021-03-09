57b07ff557606fe6009854e3b0cea2f85030f217
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-03-09 15:18:37 -0300

    Estructure inicialized

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+//import logoImage from '../images/'
 
 function Landing() {
     return (
         <div>
             <h1>ola mundo</h1>
-            <h2>reactapp sef</h2>
+            <h2>reactapp</h2>
         </div>
         
     );
