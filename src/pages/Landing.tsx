7717a8f4194b97c8f959450aceb2aed35513fcd5
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-03-09 12:21:37 -0300

    To start of front

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Landing() {
+    return (
+        <div>
+            <h1>ola mundo</h1>
+            <h2>reactapp sef</h2>
+        </div>
+        
+    );
+}
+
+export default Landing;
